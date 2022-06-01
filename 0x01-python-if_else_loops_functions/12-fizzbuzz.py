72ee428a1c1024bb8b848d9bfc872dcbd0634bbb
Aritesoma Ejuliuwa <aritesomaejuliuwa@gmail.com>
2022-06-01 19:53:27 +0000

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
+
 def fizzbuzz():
-    for n in range(1, 101):
-        if n % 3 == 0 and n % 5 == 0:
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
-        elif n % 3 == 0:
+        elif number % 3 == 0:
             print("Fizz ", end="")
-        elif n % 5 == 0:
+        elif number % 5 == 0:
             print("Buzz ", end="")
         else:
             print("{} ".format(number), end="")
